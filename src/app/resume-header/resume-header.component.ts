7c720c98e884e0521640450f63b9cf7f34cc6c70
siddhi vinayaka <svinayaka290489@gmail.com>
2019-06-20 03:28:08 +0530

    update profile

--- src/app/resume-header/resume-header.component.ts
+++ src/app/resume-header/resume-header.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResumeHeaderComponent implements OnInit {
   profileName = 'Siddhi Vinayaka';
+  profileDesignation = 'Web Application Developer';
+  profileContactDetails = [
+    { contactInfo : 8095983167, contactType: 'Phone No' },
+    { contactInfo : 'https://svinayaka.wordpress.com/', contactType: 'Blog' },
+    { contactInfo : '#105, B-block, Olive Appartment, Hoodi, Bangalore-560048', contactType: 'Address' },
+    { contactInfo : 'svinayaka290489@gmail.com', contactType: 'Email-Id' }
+  ];
   constructor() { }
 
   ngOnInit() {

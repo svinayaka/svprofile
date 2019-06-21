a94384b408b62e075cd1d349e3d533e2b71dffa4
siddhi vinayaka <svinayaka290489@gmail.com>
2019-06-21 20:57:17 +0530

    update profile

--- src/app/components/accordian-info/accordian-info.component.ts
+++ src/app/components/accordian-info/accordian-info.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, ElementRef, QueryList } from '@angular/core';
+import { element } from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-accordian-info',
@@ -6,11 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./accordian-info.component.scss']
 })
 export class AccordianInfoComponent implements OnInit {
-  title = 'Technical Skill';
-  content = `Ye to mention technical Skills`;
+  @ViewChildren('btn') accordionBtn: QueryList<ElementRef>;
+  accordionInfo = [
+    {title: 'Technical Skills', content: `Ye to mention technical Skills dsfsdffwefwefewf`},
+    {title: 'Work Experience', content: `Ye to mention technical Skills`}
+  ];
+  selectedAccordion = null;
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  ngAfterViewInit() {
+    this.hideAccordionData();
   }
 
+  selectedBtn(event) {
+    this.hideAccordionData();
+    const elm = document.activeElement.nextElementSibling;
+    (elm as any).hidden = false;
+  }
+
+  hideAccordionData() {
+    this.accordionBtn.forEach(elm => {
+        elm.nativeElement.nextElementSibling.hidden = true;
+    });
+  }
 }

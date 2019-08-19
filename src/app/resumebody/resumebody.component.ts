da32289984547702e9d306fb2f672822325aea0a
siddhi vinayaka <svinayaka290489@gmail.com>
2019-08-20 02:44:01 +0530

    updated json and added new files

--- src/app/resumebody/resumebody.component.ts
+++ src/app/resumebody/resumebody.component.ts
@@ -10,18 +10,30 @@ import { HttpClient } from '@angular/common/http';
 export class ResumebodyComponent implements OnInit {
   workInfo = [];
   skillInfo = [];
+  techInfo = [];
+  summaryInfo = [];
   constructor(private http: HttpClient, private cd: ChangeDetectorRef, private zone: NgZone) { }
 
   ngOnInit() {
-    this.http.get('./assets/workExp.json').subscribe(this.onWorkSuccess.bind(this), this.onWorkError.bind(this));
-    this.http.get('./assets/workSkill.json').subscribe(this.onSkillSuccess.bind(this), this.onSkillError.bind(this));
+    // tslint:disable-next-line:max-line-length
+    this.http.get('./assets/workExp.json').subscribe(this.onWorkSuccess.bind(this), this.onWorkError.call(this, 'Error in fetching Work Experience!'));
+    // tslint:disable-next-line:max-line-length
+    this.http.get('./assets/workSkill.json').subscribe(this.onSkillSuccess.bind(this), this.onSkillError.call(this, 'Error in fetching Skill Experience!'));
+    // tslint:disable-next-line:max-line-length
+    this.http.get('./assets/technical.json').subscribe(this.onTechSuccess.bind(this), this.onTechError.call(this, 'Error in fetching Technology!'));
+    // tslint:disable-next-line:max-line-length
+    this.http.get('./assets/summary.json').subscribe(this.onSummarySuccess.bind(this), this.onSummaryError.call(this, 'Error in fetching Summary!'));
   }
 
   onWorkSuccess(data) { this.workInfo = data.workexperience; }
-  onWorkError(error) { }
+  onWorkError(errorTxt) { }
 
-  onSkillSuccess(data) {
-    this.skillInfo = data.workSkills;
-  }
-  onSkillError(error) { }
+  onSkillSuccess(data) { this.skillInfo = data.workSkills; }
+  onSkillError(errorTxt) { }
+
+  onTechSuccess(data) { this.techInfo = data.technical; }
+  onTechError(errorTxt) { }
+
+  onSummarySuccess(data) { this.summaryInfo = data.summary; }
+  onSummaryError(errorTxt) { }
 }

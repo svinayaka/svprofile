4b98344271a0b089bbcd344f870ccb89d3a02264
siddhi vinayaka <svinayaka290489@gmail.com>
2019-10-18 09:31:14 +0530

    remove for temp purpose

--- src/app/core/client-error-interceptor/client-error-interceptor.ts
+++
@@ -1,7 +0,0 @@
-import { ErrorHandler } from '@angular/core';
-
-export class ErrorHandlers implements ErrorHandler {
-    handleError(error) {
-        console.log('I am at client side');
-    }
-}

550c36980eed0b5c021b8b45ca67a4e26fecd510
siddhi vinayaka <svinayaka290489@gmail.com>
2020-02-13 08:07:47 +0530

    added routing links

--- src/app/app-Router/main-router.ts
+++ src/app/app-Router/main-router.ts
@@ -3,8 +3,9 @@ import { HomeComponent } from '../sections/home/home.component';
 import { AboutComponent } from '../sections/about/about.component';
 
 export const route: Routes = [
-    { path: '', component: HomeComponent},
+    { path: '', redirectTo: 'home', pathMatch: 'full'},
     // { path: 'about', loadChildren: () => import('../app-Modules/aboutModules.module').then((homeMod) =>  homeMod.AboutModulesModule ) },
     // { path: 'about', loadChildren: '../app-Modules/aboutModules.module#AboutModule' },
+    { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent }
 ];
